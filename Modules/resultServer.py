a032f8b69d3fc82215297413047f1ea955026977
Calvin Dani <calvin@metamorphtech.com>
2021-07-19 00:02:48 +0530

    final changes for project submission

--- Modules/resultServer.py
+++ Modules/resultServer.py
@@ -1,14 +1,19 @@
+import csv
+from collections import defaultdict
 import webbrowser
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
-import requests
+
 
 class Serv(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
-            self.path = "./GraphicalRepresentation/ProjectOutputWebpage.html"
+            self.path = "/ProjectOutputWebpage.html"
+        elif self.path == "/?":
+            self.path = "/ProjectOutputWebpage.html"
         try:
-            file_to_open = open(self.path[:]).read()
+            print(self.path[1:])
+            file_to_open = open(self.path[1:]).read()
             self.send_response(200)
         except:
             file_to_open = "File not found"
@@ -16,21 +21,6 @@ class Serv(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(file_to_open, "utf-8"))
 
-    def do_POST(self):
-        if self.path == "/":
-            self.path = "./GraphicalRepresentation/project_output_result.html"
-        try:
-            file_to_open = open(self.path[:]).read()
-            self.send_response(200)
-        except:
-            file_to_open = "File not found"
-            self.send_response(404)
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, "utf-8"))
-
-
-if __name__ == "__main__":
-    print("SERVER STARTED")
-    httpd = HTTPServer(("localhost", 8082), Serv)
-    httpd.serve_forever()
-    
+print("SERVER STARTED")
+httpd = HTTPServer(("localhost", 8082), Serv)
+httpd.serve_forever()
